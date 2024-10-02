09dcbf9c75ffe8471358524b37abda7538594729
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2024-10-02 11:23:29 +0200

    Simplify the dark mode service

--- src/RoyDefinedComClient/src/app/services/dark-mode/darkMode.service.ts
+++ src/RoyDefinedComClient/src/app/services/dark-mode/darkMode.service.ts
@@ -10,18 +10,13 @@ import { DarkModeType } from './darkModeType';
     providedIn: 'root',
 })
 export class DarkModeService {
-    private _darkModeType?: DarkModeType;
-    private _clientPreferDarkMode?: boolean;
+    private _darkModeType: DarkModeType = 'System';
+    private _clientPreferDarkMode = false;
 
     /**
      * Returns the client's darkmode type.
      */
     public get darkModeType() {
-        // This can't be falsy if it's fetched.
-        if (!this._darkModeType) {
-            throw new Error('The dark mode type field is not set.');
-        }
-
         return this._darkModeType;
     }
 
@@ -37,56 +32,42 @@ export class DarkModeService {
         this.updateDarkMode();
     }
 
-    /**
-     * Returns `true` if the client's system prefers dark mode.
-     */
-    public get clientPreferDarkMode() {
-        if (!this._clientPreferDarkMode) {
-            throw new Error('Client prefer dark mode field should be set.');
-        }
-
-        return this._clientPreferDarkMode;
-    }
-
-    /**
-     * Returns `true` if dark mode is enabled.
-     */
-    public get darkModeEnabled() {
-        return this.darkModeType === 'Dark' || (this.darkModeType === 'System' && this._clientPreferDarkMode);
-    }
-
     constructor(private readonly _clientSettingsService: ClientSettingsService) {}
 
     /**
      * Initializes the service.
      */
     public initialize() {
-        // On initialization this service will start tracking the user's system settings to see if dark mode is preferred.
-        // On change, the application will adjust so this setting is respected.
+        const darkModeType = this._clientSettingsService.settings?.darkModeType;
+        if (darkModeType) {
+            this._darkModeType = darkModeType;
+        }
+
+        this.initializeDarkModeListener();
+        this.updateDarkMode();
+    }
+
+    private initializeDarkModeListener() {
         const preferColorSchemeDarkMedia = window.matchMedia('(prefers-color-scheme: dark)');
         this._clientPreferDarkMode = preferColorSchemeDarkMedia.matches;
-        this._darkModeType = this._clientSettingsService.settings?.darkModeType || 'System';
-        this.updateDarkMode();
 
         preferColorSchemeDarkMedia.addEventListener('change', (event) => {
-            console.log('Dark mode service observed change.');
             this._clientPreferDarkMode = event.matches;
             this.updateDarkMode();
         });
-
-        console.log('Dark mode service initialized.');
     }
 
     // Updates dark mode when system options change preferation or something updated the current darkmode value.
     private updateDarkMode() {
         const darkModeClassAdded = document.documentElement.classList.contains('dark');
+        const darkModeEnabled = this.darkModeType === 'Dark' || (this.darkModeType === 'System' && this._clientPreferDarkMode);
 
-        if (darkModeClassAdded && !this.darkModeEnabled) {
+        if (darkModeClassAdded && !darkModeEnabled) {
             document.documentElement.classList.remove('dark');
             return;
         }
 
-        if (!darkModeClassAdded && this.darkModeEnabled) {
+        if (!darkModeClassAdded && darkModeEnabled) {
             document.documentElement.classList.add('dark');
             return;
         }
