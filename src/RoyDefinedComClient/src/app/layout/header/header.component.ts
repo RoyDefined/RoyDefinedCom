536b82ed5af94cd9748979c7b6bd75cf927a1c8b
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2024-10-02 11:46:35 +0200

    Move dark mode svg icons to their own components

--- src/RoyDefinedComClient/src/app/layout/header/header.component.ts
+++ src/RoyDefinedComClient/src/app/layout/header/header.component.ts
@@ -2,12 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DarkModeService } from '../../services/dark-mode/darkMode.service';
+import { DarkModeMoonComponent } from '../../components/svg/dark-mode-moon.component';
+import { DarkModeSunComponent } from '../../components/svg/dark-mode-sun.component';
+import { DarkModeSystemComponent } from '../../components/svg/dark-mode-system.component';
 
 @Component({
     standalone: true,
     selector: 'app-header',
     templateUrl: './header.component.html',
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, DarkModeSystemComponent, DarkModeSunComponent, DarkModeMoonComponent],
 })
 export class HeaderComponent {
     private readonly _darkModeService = inject(DarkModeService);
