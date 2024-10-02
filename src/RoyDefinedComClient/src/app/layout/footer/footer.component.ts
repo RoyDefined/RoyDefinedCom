260a0ce7dee967e1633493eaf0d989185e2af045
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2024-10-02 12:37:11 +0200

    Replace social icons with svg images

--- src/RoyDefinedComClient/src/app/layout/footer/footer.component.ts
+++ src/RoyDefinedComClient/src/app/layout/footer/footer.component.ts
@@ -5,12 +5,23 @@ import { DarkModeMoonComponent } from '../../components/svg/dark-mode-moon.compo
 import { DarkModeSunComponent } from '../../components/svg/dark-mode-sun.component';
 import { DarkModeSystemComponent } from '../../components/svg/dark-mode-system.component';
 import { DarkModeService } from '../../services/dark-mode/darkMode.service';
+import { SocialGithubComponent } from '../../components/svg/social-github.component';
+import { SocialDiscordComponent } from '../../components/svg/social-discord.component';
 
 @Component({
     standalone: true,
     selector: 'app-footer',
     templateUrl: './footer.component.html',
-    imports: [CommonModule, RouterModule, DarkModeSystemComponent, DarkModeSunComponent, DarkModeMoonComponent, NgOptimizedImage],
+    imports: [
+        CommonModule,
+        RouterModule,
+        DarkModeSystemComponent,
+        DarkModeSunComponent,
+        DarkModeMoonComponent,
+        SocialGithubComponent,
+        SocialDiscordComponent,
+        NgOptimizedImage,
+    ],
 })
 export class FooterComponent {
     private readonly _darkModeService = inject(DarkModeService);
