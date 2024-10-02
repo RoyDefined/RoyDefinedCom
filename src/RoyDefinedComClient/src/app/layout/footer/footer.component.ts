e8fd2e865552505d01744ae7d9783467cc301554
RoyDefined <145583009+RoyDefined@users.noreply.github.com>
2024-10-02 12:15:53 +0200

    Move the dark mode button to the footer and remove the header

--- src/RoyDefinedComClient/src/app/layout/footer/footer.component.ts
+++ src/RoyDefinedComClient/src/app/layout/footer/footer.component.ts
@@ -1,11 +1,27 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DarkModeMoonComponent } from '../../components/svg/dark-mode-moon.component';
+import { DarkModeSunComponent } from '../../components/svg/dark-mode-sun.component';
+import { DarkModeSystemComponent } from '../../components/svg/dark-mode-system.component';
+import { DarkModeService } from '../../services/dark-mode/darkMode.service';
 
 @Component({
     standalone: true,
     selector: 'app-footer',
     templateUrl: './footer.component.html',
-    imports: [CommonModule, RouterModule, NgOptimizedImage],
+    imports: [CommonModule, RouterModule, DarkModeSystemComponent, DarkModeSunComponent, DarkModeMoonComponent, NgOptimizedImage],
 })
-export class FooterComponent {}
+export class FooterComponent {
+    private readonly _darkModeService = inject(DarkModeService);
+
+    public get darkModeType() {
+        return this._darkModeService.darkModeType;
+    }
+
+    public nextDarkModeType() {
+        const currentDarkModeType = this.darkModeType();
+        const nextType = currentDarkModeType === 'System' ? 'Light' : currentDarkModeType === 'Light' ? 'Dark' : 'System';
+        this._darkModeService.setDarkModeType(nextType);
+    }
+}
